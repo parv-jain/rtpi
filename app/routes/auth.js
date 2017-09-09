efbf95ca4a1f36c65ba32783131e3cf546bbb087
Parv Jain <parvjn616@gmail.com>
2017-09-09 20:59:38 +0530

    seperated checkAuth module

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,13 +1,5 @@
 var passport = require('passport');
-var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	if (req.isAuthenticated())
-		return next();
-	// if the user is not authenticated then redirect him to the login page
-	res.redirect('/auth');
-}
+var isAuthenticated = require('../modules/checkAuth');
 
 
 module.exports = function(app){
