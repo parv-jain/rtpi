7e8112b56f564ac5a6fbc1c2df51c892753522b9
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2017-09-11 18:01:43 +0530

    Removed extra comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 // add all rotes here
-module.exports = function(app){
-  module.exports = require('./auth.js')(app);
-  module.exports = require('./scrapping.js')(app);
-  module.exports = require('./mail.js')(app);
+module.exports = function(app) {
+    module.exports = require('./auth.js')(app);
+    module.exports = require('./amazon.js')(app);
+    module.exports = require('./flipkart.js')(app);
+    module.exports = require('./mail.js')(app);
 }
